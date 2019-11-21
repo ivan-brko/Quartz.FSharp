d3e25ed9f0273d70d73f542a438dfa90b067d991
Ivan Brko <ivan.brko@protonmail.com>
2019-11-21 12:12:58 +0100

    Demo app for Unrecoverable job Added a simple example of using unrecoverable job

---
+++ Quartz.FSharp.Unrecoverable.Demo/Program.fs
@@ -0,0 +1,21 @@
+open System
+open Quartz.Fsharp
+
+let getTimestamp() = DateTime.Now.ToString("HH:mm:ss")
+
+[<EntryPoint>]
+let main argv =
+    printfn "Running a demo of Quartz.FSharp unrecoverable job"
+    let cron = Cron.CreateDefaultCron() |> Cron.SetEverySecond
+    use context = new Context.QuartzSchedulingContext()
+    let schedulingResult =
+        UnrecoverableJob.ScheduleJobForCommonGroup context cron
+            Context.TaskPriority.Medium "Example Task"
+            (fun () -> printfn "%s: Task Called" (getTimestamp()))
+
+    match schedulingResult with
+    | Ok(_) -> Context.StartRunningScheduledTasks context        
+    | Error(_) -> printfn "Error while trying to schedule task"
+
+    let line = Console.ReadLine() |> ignore
+    0 // return an integer exit code
