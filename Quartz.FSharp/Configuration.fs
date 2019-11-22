7bd948238068d5e601168efd1e0a9d48b9ec300a
Ivan Brko <ivan.brko@protonmail.com>
2019-11-22 20:59:59 +0100

    Reordering params

--- Quartz.FSharp/Configuration.fs
+++ Quartz.FSharp/Configuration.fs
@@ -22,10 +22,10 @@ module Configuration =
     /// Sets a thread count for Quartz configuration.
     /// Does not modify the properties it receives, but creates new properties.
     /// </summary>
-    /// <param name="properties">Quartz configuration whose fields will be copied in the result configuration</param>
     /// <param name="threadCount">Wanted thread count</param>
+    /// <param name="properties">Quartz configuration whose fields will be copied in the result configuration</param>
     /// <returns> If threads are in allowed range (between 1 and 20), returns Ok with new configuration, otherwise returns an Error</returns>
-    let SetThreadCount (QuartzConfiguration properties) threadCount =
+    let SetThreadCount threadCount (QuartzConfiguration properties) =
         if threadCount >= 1 && threadCount <= 20 then
             let newProperties = NameValueCollection(properties)
             newProperties.Set("quartz.threadPool.threadCount", threadCount.ToString())
@@ -38,10 +38,10 @@ module Configuration =
     /// <summary>
     /// Sets the name for scheduler instance. Refer to Quartz.Net documentation for more information about instance names
     /// </summary>
-    /// <param name="properties">Quartz configuration whose fields will be copied in the result configuration</param>
     /// <param name="schedulerInstanceName">Wanted instance name</param>
+    /// <param name="properties">Quartz configuration whose fields will be copied in the result configuration</param>
     /// <returns> New configuration with instance name set to wanted value</returns>
-    let SetSchedulerInstanceName (QuartzConfiguration properties) schedulerInstanceName =
+    let SetSchedulerInstanceName schedulerInstanceName (QuartzConfiguration properties) =
         let newProperties = NameValueCollection(properties)
         newProperties.Set("quartz.scheduler.instanceName", schedulerInstanceName)
         QuartzConfiguration newProperties
