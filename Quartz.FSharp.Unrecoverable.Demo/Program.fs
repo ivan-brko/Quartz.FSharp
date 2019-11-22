20230ce9ffe9c6b8fa308b28075a28fb9276f244
Ivan Brko <ivan.brko@protonmail.com>
2019-11-22 17:42:45 +0100

    Added comments to the example program

--- Quartz.FSharp.Unrecoverable.Demo/Program.fs
+++ Quartz.FSharp.Unrecoverable.Demo/Program.fs
@@ -6,25 +6,59 @@ let getTimestamp() = DateTime.Now.ToString("HH:mm:ss")
 [<EntryPoint>]
 let main argv =
     printfn "Running a demo of Quartz.FSharp unrecoverable job"
+
+    // create a cron that will run the task every second
     let cron = Cron.CreateDefaultCron() |> Cron.SetEverySecond
+
+    // create new context without any configuration
     use context = new Context.QuartzSchedulingContext()
     
+    // the logging function
+    // level gives the level of logging message (DEBUG, INFO...), funOpt is optional that, 
+    // if set to Some, will contain a function that will generate the message. 
+    // Last two args (not used here) are exception and params. 
+    // Exception will contain the exception if it has occured so that it can be logged 
+    // and params carry some additional data. Refer to LibLog docs for more information
     let loggerFunction level funOpt _ _ = 
         match funOpt with
         | Some f -> printfn "Logger: %A: %s" level (f())
                     true
         | None -> true            
 
+    // Use the logging function created above
     Logging.SetQuartzLoggingFunction loggerFunction    
 
+    // Schedule the task
     let schedulingResult =
         UnrecoverableJob.ScheduleJobForCommonGroup context cron
             Context.TaskPriority.Medium "Example Task"
             (fun () -> printfn "%s: Task Called" (getTimestamp()))
 
+    // If scheduling was OK, start running the task
     match schedulingResult with
     | Ok(_) -> Context.StartRunningScheduledTasks context        
     | Error(_) -> printfn "Error while trying to schedule task"
 
+    // Wait for <enter> to complete the program
     let line = Console.ReadLine() |> ignore
     0 // return an integer exit code
+
+
+//the output should contain lines like this, where lines starting with timestamp are the result of task being called
+// and other lines are reported by the logger
+
+// 17:41:04: Task Called
+// Logger: Debug: Trigger instruction : NoInstruction
+// Logger: Debug: Producing instance of Job 'groupAll.Example Task', class=Quartz.Fsharp.UnrecoverableJob+WrapperJob
+// Logger: Debug: Calling Execute on job groupAll.Example Task
+// Logger: Debug: Batch acquisition of {0} triggers
+// 17:41:05: Task Called
+// Logger: Debug: Trigger instruction : NoInstruction
+// Logger: Debug: Producing instance of Job 'groupAll.Example Task', class=Quartz.Fsharp.UnrecoverableJob+WrapperJob
+// Logger: Debug: Batch acquisition of {0} triggersLogger: Debug: 
+// Calling Execute on job groupAll.Example Task
+// 17:41:06: Task Called
+// Logger: Debug: Trigger instruction : NoInstruction
+// Logger: Debug: Producing instance of Job 'groupAll.Example Task', class=Quartz.Fsharp.UnrecoverableJob+WrapperJob
+// Logger: Debug: Calling Execute on job groupAll.Example TaskLogger: Debug: Batch acquisition of {0} triggers
+
